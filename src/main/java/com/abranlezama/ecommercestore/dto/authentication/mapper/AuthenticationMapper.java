cfe83c10c7ce51f90eb112937fedcd4ff5894638
Abran Lezama <lezama.abran@gmail.com>
2023-05-11 17:49:22 -0700

    Changed name of method that maps from CustomerRegisterDto to User

--- src/main/java/com/abranlezama/ecommercestore/dto/authentication/mapper/AuthenticationMapper.java
+++ src/main/java/com/abranlezama/ecommercestore/dto/authentication/mapper/AuthenticationMapper.java
@@ -4,13 +4,12 @@ import com.abranlezama.ecommercestore.dto.authentication.RegisterCustomerDTO;
 import com.abranlezama.ecommercestore.model.Customer;
 import com.abranlezama.ecommercestore.model.User;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 @Mapper
 public interface AuthenticationMapper {
 
 
-    User mapToEntity(RegisterCustomerDTO dto);
+    User mapToUser(RegisterCustomerDTO dto);
 
     Customer mapToCustomer(RegisterCustomerDTO dto);
 }
--- src/test/java/com/abranlezama/ecommercestore/dto/authentication/mapper/AuthenticationMapperTest.java
+++ src/test/java/com/abranlezama/ecommercestore/dto/authentication/mapper/AuthenticationMapperTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.Test;
 import org.mapstruct.factory.Mappers;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class AuthenticationMapperTest {
 
@@ -20,7 +19,7 @@ class AuthenticationMapperTest {
         RegisterCustomerDTO dto = RegisterCustomerDTO.builder().build();
 
         // When
-        User user = mapper.mapToEntity(dto);
+        User user = mapper.mapToUser(dto);
 
         // Then
         assertThat(user.getEmail()).isEqualTo(dto.email());

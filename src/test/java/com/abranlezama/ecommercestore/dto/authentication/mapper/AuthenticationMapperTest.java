76690467411d6fc9bd014e57353cd0aa7f3a8037
Abran Lezama <lezama.abran@gmail.com>
2023-05-11 17:46:53 -0700

    Tested correct convertion from RegisterCustomerDTO to customer

--- src/test/java/com/abranlezama/ecommercestore/dto/authentication/mapper/AuthenticationMapperTest.java
+++ src/test/java/com/abranlezama/ecommercestore/dto/authentication/mapper/AuthenticationMapperTest.java
@@ -1,7 +1,9 @@
 package com.abranlezama.ecommercestore.dto.authentication.mapper;
 
 import com.abranlezama.ecommercestore.dto.authentication.RegisterCustomerDTO;
+import com.abranlezama.ecommercestore.model.Customer;
 import com.abranlezama.ecommercestore.model.User;
+import com.abranlezama.ecommercestore.objectmother.RegisterCustomerDTOMother;
 import org.junit.jupiter.api.Test;
 import org.mapstruct.factory.Mappers;
 
@@ -24,6 +26,25 @@ class AuthenticationMapperTest {
         assertThat(user.getEmail()).isEqualTo(dto.email());
         assertThat(user.getPassword()).isEqualTo(dto.password());
         assertThat(user.getId()).isEqualTo(null);
+    }
+
+    @Test
+    void shouldAssignPropertiesFromCustomerRegisterDtoToCustomer() {
+        // Given
+        RegisterCustomerDTO dto = RegisterCustomerDTOMother.complete().build();
+
+        // When
+        Customer customer = mapper.mapToCustomer(dto);
+
+        // Then
+        assertThat(customer.getFirstName()).isEqualTo(dto.firstName());
+        assertThat(customer.getLastName()).isEqualTo(dto.lastName());
+        assertThat(customer.getStreet()).isEqualTo(dto.street());
+        assertThat(customer.getCity()).isEqualTo(dto.city());
+        assertThat(customer.getState()).isEqualTo(dto.state());
+        assertThat(customer.getPostalCode()).isEqualTo(dto.postalCode());
+        assertThat(customer.getId()).isEqualTo(null);
+
     }
 
 }

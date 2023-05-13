8db50999f4dcecd027ee9076b35015c37199859c
Abran Lezama <lezama.abran@gmail.com>
2023-05-12 17:41:33 -0700

    Whitelisted products url - no authentication required

--- src/main/java/com/abranlezama/ecommercestore/config/SecurityConfiguration.java
+++ src/main/java/com/abranlezama/ecommercestore/config/SecurityConfiguration.java
@@ -19,6 +19,7 @@ public class SecurityConfiguration {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(request -> {
+                    request.requestMatchers("/products").permitAll();
                     request.requestMatchers("/auth/**").permitAll();
                     request.anyRequest().authenticated();
                 });

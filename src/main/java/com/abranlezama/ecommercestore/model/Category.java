3584f4472c23045dff6b0ca77cd300bc3486f380
Abran Lezama <lezama.abran@gmail.com>
2023-05-13 23:57:59 -0700

    Added Lombock methods to Category and ProductCategoryId
    - Added annotations for getters, setters, constructors, toString

--- src/main/java/com/abranlezama/ecommercestore/model/Category.java
+++ src/main/java/com/abranlezama/ecommercestore/model/Category.java
@@ -1,11 +1,19 @@
 package com.abranlezama.ecommercestore.model;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.List;
+import java.util.Objects;
 
 @Entity
 @Table(name = "categories")
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Getter
+@Setter
+@Builder
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -15,5 +23,19 @@ public class Category {
     private CategoryType category;
 
     @ManyToMany(mappedBy = "productCategories")
+    @ToString.Exclude
     List<Product> products;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return Objects.equals(id, category.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
--- src/main/java/com/abranlezama/ecommercestore/model/ProductCategoryId.java
+++ src/main/java/com/abranlezama/ecommercestore/model/ProductCategoryId.java
@@ -2,10 +2,17 @@ package com.abranlezama.ecommercestore.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import lombok.*;
 
 import java.io.Serializable;
 
 @Embeddable
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Getter
+@Setter
+@Builder
 public class ProductCategoryId implements Serializable {
 
     @Column(name = "product_id")

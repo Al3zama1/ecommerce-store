e206e60958d5cdcb9cdc1f1e5ded588ae8ef8bb2
Abran Lezama <lezama.abran@gmail.com>
2023-05-13 21:23:27 -0700

    Added product categories database migration

--- src/main/resources/db/migration/V007__create_product_category_table.sql
+++ src/main/resources/db/migration/V007__create_product_category_table.sql
@@ -1,9 +1,7 @@
 CREATE TABLE product_categories(
-    id INTEGER GENERATED ALWAYS AS IDENTITY,
     product_id BIGINT NOT NULL,
     category_id INTEGER NOT NULL,
     FOREIGN KEY (product_id) REFERENCES products(id),
     FOREIGN KEY (category_id) REFERENCES categories(id),
-    UNIQUE (product_id, category_id),
-    PRIMARY KEY (id)
+    PRIMARY KEY (product_id, category_id)
 )

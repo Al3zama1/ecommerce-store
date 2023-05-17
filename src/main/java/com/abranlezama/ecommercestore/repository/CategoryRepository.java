0aad0f21a82c38a4d8526e49d4c2b22ef96b5146
Abran Lezama <lezama.abran@gmail.com>
2023-05-16 19:50:49 -0700

    Return all categories whose type is in the set of CategoryType passed

--- src/main/java/com/abranlezama/ecommercestore/repository/CategoryRepository.java
+++ src/main/java/com/abranlezama/ecommercestore/repository/CategoryRepository.java
@@ -5,10 +5,14 @@ import com.abranlezama.ecommercestore.model.CategoryType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
 
     Optional<Category> findByCategory(CategoryType categoryType);
+
+    Set<Category> findAllByCategoryIn(Set<CategoryType> categories);
 }

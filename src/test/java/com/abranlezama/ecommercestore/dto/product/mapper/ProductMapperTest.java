84b88f2112424dd725bb6677b391b86479ade057
Abran Lezama <lezama.abran@gmail.com>
2023-05-16 22:15:17 -0700

    Tested convertion of addProductRequest to product entity/model

--- src/test/java/com/abranlezama/ecommercestore/dto/product/mapper/ProductMapperTest.java
+++ src/test/java/com/abranlezama/ecommercestore/dto/product/mapper/ProductMapperTest.java
@@ -1,7 +1,9 @@
 package com.abranlezama.ecommercestore.dto.product.mapper;
 
+import com.abranlezama.ecommercestore.dto.product.AddProductRequestDTO;
 import com.abranlezama.ecommercestore.dto.product.ProductResponseDTO;
 import com.abranlezama.ecommercestore.model.Product;
+import com.abranlezama.ecommercestore.objectmother.AddProductRequestDTOMother;
 import com.abranlezama.ecommercestore.objectmother.ProductMother;
 import com.abranlezama.ecommercestore.objectmother.ProductResponseDTOMother;
 import org.junit.jupiter.api.Test;
@@ -39,6 +41,21 @@ class ProductMapperTest {
 
         // Then
         assertThat(dtoResponse).isNull();
+    }
+
+    @Test
+    void shouldConvertRequestToAddProductToProductEntity() {
+        // Given
+        AddProductRequestDTO requestDto = AddProductRequestDTOMother.create().build();
+
+        // When
+        Product product = mapper.mapAddProductRequestToEntity(requestDto);
+
+        // Then
+        assertThat(product.getName()).isEqualTo(requestDto.name());
+        assertThat(product.getDescription()).isEqualTo(requestDto.description());
+        assertThat(product.getPrice()).isEqualTo(requestDto.price());
+        assertThat(product.getStockQuantity()).isEqualTo(requestDto.stockQuantity());
     }
 
 }

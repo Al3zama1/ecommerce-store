43488d0dcbd8a926893903fc281619ff3ee880d6
Abran Lezama <lezama.abran@gmail.com>
2023-05-16 22:45:48 -0700

    Added user with role of employee using commandlinerunner for testing purposes

--- src/main/java/com/abranlezama/ecommercestore/Application.java
+++ src/main/java/com/abranlezama/ecommercestore/Application.java
@@ -1,13 +1,43 @@
 package com.abranlezama.ecommercestore;
 
+import com.abranlezama.ecommercestore.model.Role;
+import com.abranlezama.ecommercestore.model.RoleType;
+import com.abranlezama.ecommercestore.model.User;
+import com.abranlezama.ecommercestore.repository.RoleRepository;
+import com.abranlezama.ecommercestore.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Set;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(UserRepository userRepository,
+                                        PasswordEncoder passwordEncoder,
+                                        RoleRepository roleRepository) {
+        return (args) -> {
+            Role employeeRole = roleRepository.findByRole(RoleType.EMPLOYEE).orElseThrow();
+            String employeeEmail = "employee@gmail.com";
+            if (userRepository.findByEmail(employeeEmail).isEmpty()) {
+                User employee = User.builder()
+                        .email(employeeEmail)
+                        .isEnabled(true)
+                        .password(passwordEncoder.encode("12345678"))
+                        .roles(Set.of(employeeRole))
+                        .build();
+
+                userRepository.save(employee);
+            }
+        };
     }
 
 }

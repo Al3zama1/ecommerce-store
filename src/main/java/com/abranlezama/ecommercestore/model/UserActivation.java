4a0b96c1294faddaee29958501ec5d32afed74c4
Abran Lezama <lezama.abran@gmail.com>
2023-05-19 16:18:39 -0700

    Added reference to user in the user activation table
    User reference is needed to be able to set its enabled status to true.

--- src/main/java/com/abranlezama/ecommercestore/model/UserActivation.java
+++ src/main/java/com/abranlezama/ecommercestore/model/UserActivation.java
@@ -1,11 +1,19 @@
 package com.abranlezama.ecommercestore.model;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 import java.util.UUID;
 
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Getter
+@Setter
+@Builder
 public class UserActivation {
 
     @Id
@@ -14,4 +22,19 @@ public class UserActivation {
     @Column(nullable = false)
     private LocalDateTime createdDate;
     private LocalDateTime expirationDate;
+    @OneToOne
+    private User user;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserActivation that = (UserActivation) o;
+        return Objects.equals(token, that.token);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(token);
+    }
 }
--- src/main/resources/db/migration/V012__create_user_activation_table.sql
+++ src/main/resources/db/migration/V012__create_user_activation_table.sql
@@ -2,5 +2,7 @@ CREATE TABLE user_activation(
     token UUID,
     created_date TIMESTAMP NOT NULL,
     expiration_date TIMESTAMP,
+    user_id BIGINT UNIQUE NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (id),
     PRIMARY KEY (token)
 );

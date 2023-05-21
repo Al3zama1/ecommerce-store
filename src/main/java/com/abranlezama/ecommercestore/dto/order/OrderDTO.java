2abcc9fc7dd453693172eb42c5cfeaf37b5da296
Abran Lezama <lezama.abran@gmail.com>
2023-05-21 01:27:26 -0700

    Refactored OrderResponseDTO to OrderDTO

--- src/main/java/com/abranlezama/ecommercestore/dto/order/OrderDTO.java
+++ src/main/java/com/abranlezama/ecommercestore/dto/order/OrderDTO.java
@@ -5,7 +5,7 @@ import lombok.Builder;
 import java.time.LocalDateTime;
 
 @Builder
-public record OrderResponseDTO(
+public record OrderDTO(
         Long id,
         Float totalCost,
         String orderStatus,

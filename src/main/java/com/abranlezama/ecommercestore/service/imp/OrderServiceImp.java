89e5aa130f8f65a328b69bba93860ad49cca7251
Abran Lezama <lezama.abran@gmail.com>
2023-05-21 00:10:28 -0700

    Added pagination support

--- src/main/java/com/abranlezama/ecommercestore/service/imp/OrderServiceImp.java
+++ src/main/java/com/abranlezama/ecommercestore/service/imp/OrderServiceImp.java
@@ -1,15 +1,32 @@
 package com.abranlezama.ecommercestore.service.imp;
 
 import com.abranlezama.ecommercestore.dto.order.OrderResponseDTO;
+import com.abranlezama.ecommercestore.dto.order.mapper.OrderMapper;
+import com.abranlezama.ecommercestore.model.Order;
+import com.abranlezama.ecommercestore.repository.OrderRepository;
 import com.abranlezama.ecommercestore.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
+@RequiredArgsConstructor
 public class OrderServiceImp implements OrderService {
+
+    private final OrderRepository orderRepository;
+    private final OrderMapper orderMapper;
+
     @Override
-    public Set<OrderResponseDTO> getCustomerOrders(String userEmail) {
-        return null;
+    public Set<OrderResponseDTO> getCustomerOrders(String userEmail, int page, int pageSize) {
+        Page<Order> orders = orderRepository
+                .findAllByCustomer_User_Email(PageRequest.of(page, pageSize), userEmail);
+
+        return orders.stream()
+                .map(orderMapper::mapOrderToDto)
+                .collect(Collectors.toSet());
     }
 }

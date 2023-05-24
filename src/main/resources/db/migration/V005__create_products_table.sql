83d8ab97da70e8cd124ac2e11ca28f8c89eb9761
Abran Lezama <lezama.abran@gmail.com>
2023-05-24 14:52:26 -0700

    Added base products to the product sql table migration.

--- src/main/resources/db/migration/V005__create_products_table.sql
+++ src/main/resources/db/migration/V005__create_products_table.sql
@@ -6,3 +6,11 @@ CREATE TABLE products(
     stock_quantity SMALLINT NOT NULL,
     PRIMARY KEY (id)
 );
+
+INSERT INTO products (name, description, price, stock_quantity)
+VALUES
+    ('iPhone 12', 'A high-end smartphone with advanced features.', 999.99, 50),
+    ('Samsung Galaxy S21', 'A flagship Android smartphone with a powerful camera.', 899.99, 30),
+    ('MacBook Pro', 'A premium laptop for professional users.', 1999.99, 20),
+    ('Sony PlayStation 5', 'A next-generation gaming console for immersive gaming experiences.', 499.99, 10),
+    ('Bose QuietComfort 35 II', 'Wireless noise-canceling headphones for excellent audio quality.', 299.99, 15);

273847778ec9403b6d03426b9c6fbe9b10c7d617
Abran Lezama <lezama.abran@gmail.com>
2023-05-24 16:26:30 -0700

    Created cart and cart item repositories

---
+++ src/main/java/com/abranlezama/ecommercestore/cart/CartItemRepository.java
@@ -0,0 +1,8 @@
+package com.abranlezama.ecommercestore.cart;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CartItemRepository extends JpaRepository<CartItem, CartItemId> {
+}

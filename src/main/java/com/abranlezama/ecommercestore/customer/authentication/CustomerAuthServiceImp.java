db63ab52666b3a9f934bf99c302271e11891cc38
Abran Lezama <lezama.abran@gmail.com>
2023-05-24 17:34:22 -0700

    Added transactional annotations to service methods

--- src/main/java/com/abranlezama/ecommercestore/customer/authentication/CustomerAuthServiceImp.java
+++ src/main/java/com/abranlezama/ecommercestore/customer/authentication/CustomerAuthServiceImp.java
@@ -15,8 +15,10 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class CustomerAuthServiceImp implements CustomerAuthService {
 
--- src/main/java/com/abranlezama/ecommercestore/product/ProductServiceImp.java
+++ src/main/java/com/abranlezama/ecommercestore/product/ProductServiceImp.java
@@ -6,10 +6,12 @@ import com.abranlezama.ecommercestore.product.dto.ProductDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class ProductServiceImp implements ProductService{
 
